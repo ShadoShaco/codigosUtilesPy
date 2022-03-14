e937e4b8e13834f5d1b1b6af4c39781c7da76108
ShadoShaco <39597650+ShadoShaco@users.noreply.github.com>
2022-03-14 00:26:19 -0300

    Add files via upload

---
+++ estractWebLinks.py
@@ -0,0 +1,15 @@
+#pip install beautifulsoup4
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://copyassingnment.com/author/admin/'
+
+res = requests.get(url).content
+
+soup = BeautifulSoup(res,'html.parser')
+
+links = soup.find_all('a')
+
+for link in links:
+	print(link['href'])
---
+++ instagramInfo.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+username = input("Enter Instagram username: ")
+
+res = requests.get("http://instgram.com" + username)
+soup_data = BeautifulSoup(res.text, "html.parser")
+userInfo = soup_data.find("meta", property = "og:description")
+
+print(userInfo.attrs["content"][:40])
