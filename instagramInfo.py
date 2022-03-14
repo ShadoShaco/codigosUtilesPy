802e8dba5c218085ee68be3f5bed14920e696a1a
ShadoShaco <39597650+ShadoShaco@users.noreply.github.com>
2022-03-14 00:28:11 -0300

    Update instagramInfo.py

--- instagramInfo.py
+++ instagramInfo.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 username = input("Enter Instagram username: ")
-
+ 
 res = requests.get("http://instgram.com" + username)
 soup_data = BeautifulSoup(res.text, "html.parser")
 userInfo = soup_data.find("meta", property = "og:description")
