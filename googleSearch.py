e2d71059e1b666240b9ee89a402ff0ad7f2f9623
ShadoShaco <39597650+ShadoShaco@users.noreply.github.com>
2022-03-14 00:27:32 -0300

    Update googleSearch.py

--- googleSearch.py
+++ googleSearch.py
@@ -14,4 +14,4 @@ browser = webdriver.Chrome('chromedriver')
   
 for i in range(1):
     matched_elements = browser.get("https://www.google.com/search?q=" +
-                                     search_string + "&start=" + str(i))
+                                     search_string + "&start=" + str(i)) 
